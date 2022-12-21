e3df30a6213c0ef757562c575a1520b2f2a9c70e
NickWhit1 <nwhabibulen@yahoo.com>
2022-12-20 21:40:34 -0600

    9.2/9.3 Updates

--- server/routers/pizzas.js
+++ server/routers/pizzas.js
@@ -26,4 +26,35 @@ router.get("/:id", (request, response) => {
   });
 });
 
+router.delete("/:id", (request, response) => {
+  Pizza.findByIdAndRemove(request.params.id, {}, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+router.put("/:id", (request, response) => {
+  const body = request.body;
+  Pizza.findByIdAndUpdate(
+    request.params.id,
+    {
+      $set: {
+        // Take note that the customer is not included, so it can't
+        crust: body.crust,
+        cheese: body.cheese,
+        sauce: body.sauce,
+        toppings: body.toppings
+      }
+    },
+    {
+      new: true,
+      upsert: true
+    },
+    (error, record) => {
+      if (error) return response.status(500).json(error);
+      return response.json(record);
+    }
+  );
+});
+
 module.exports = router;
